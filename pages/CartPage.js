506bdbfc1b3566f4b76e30760286eab266b9eafd
lubna331 <lubna1077@gmail.com>
2025-09-25 01:19:41 +0600

    Project Done

--- pages/CartPage.js
+++ pages/CartPage.js
@@ -8,9 +8,6 @@ class CartPage {
         this.nameInput = "//input[@id='billing_first_name']";
         this.phoneInput = "//input[@id='billing_phone']";
         this.emailInput = "//input[@id='billing_email']";
-        this.districtDropdown = '#select2-billing_state-container';
-        this.districtOption = 'ul.select2-results__options li';
-        this.areaInput = "billing_area";
         this.addressInput = "//textarea[@id='billing_address_1']";
     }
 
@@ -25,11 +22,7 @@ class CartPage {
         await this.page.fill(this.emailInput, email);
         await this.page.fill(this.addressInput, address);
 
-        // District (custom Select2 dropdown)
-        await this.page.click(this.districtDropdown);
-        await this.page.click(this.districtOption);
-        await this.page.getByRole('option', { name: 'Cumilla' }).click();
-        // Area (normal <select>)
-        await this.page.selectOption(this.areaInput, '492');
+        await this.page.selectOption('#billing_state', 'BD-13');
+        await this.page.selectOption('#billing_area', '492');
     }
 }
